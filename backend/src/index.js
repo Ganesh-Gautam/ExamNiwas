fd804f896c5209cb76603822a611b4fc2970fb0e
Ganesh-Gautam <161460776+Ganesh-Gautam@users.noreply.github.com>
2026-06-12 12:40:32 +0530

    error

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,12 +7,7 @@ dotenv.config({
 })
 
  
-connectDB()
-.then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
-        console.log(`Server is running at PORT : ${process.env.PORT}`)
-    }) 
-})
+await connectDB()
 .catch((err)=>{
     console.log("MONGO db connection failed !!! ", err)
 })
