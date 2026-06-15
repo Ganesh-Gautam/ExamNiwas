4145c07980f9477f55d17fcc3358cee65cc8be2c
Ganesh-Gautam <161460776+Ganesh-Gautam@users.noreply.github.com>
2026-06-15 12:10:56 +0530

    fix link

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { extractApiErrorMessage } from "../utils/apiError.js";
 
 const axiosInstance = axios.create({
-  baseURL: "https://examniwas.vercel.app/api/v1",
+  baseURL:"https://examniwas.onrender.com/api/v1",
   withCredentials: true
 });
 
